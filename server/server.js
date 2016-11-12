f01457ae79aa1eafc9d8b8be26e080190023420e
Dariusz Mikołajczyk <dariusz.mikolajczyk@partnertech.pl>
2016-11-12 23:22:23 +0100

    dodanie obsługi błędów serwera

--- server/server.js
+++ server/server.js
@@ -219,7 +219,28 @@ app.use('/', function (req, res) {
 var port = 3001;
 
 console.log('express server ready.');
-app.listen(port, function(error) {
-  if (error) throw error;
-  console.log("Express server listening on port", port);
-});
+app.listen(port, onError);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}

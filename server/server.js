89fd931787e12a0a6203c9d9540945c16815909e
Dariusz Mikołajczyk <dariusz.mikolajczyk@partnertech.pl>
2016-11-12 23:57:10 +0100

    port error handler on server.js

--- server/server.js
+++ server/server.js
@@ -294,3 +294,18 @@ function onListening() {
 }
 
 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}

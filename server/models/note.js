b0e8688b5ecc8a8df60c5e2362cefa9df11228af
Dariusz Mikołajczyk <damii@poczta.onet.pl>
2018-12-24 14:12:43 +0100

    last commit

--- server/models/note.js
+++ server/models/note.js
@@ -1,16 +1,14 @@
-'use strict';
-
 var mongoose = require('mongoose');
 //http://adrianmejia.com/blog/2014/10/01/creating-a-restful-api-tutorial-with-nodejs-and-mongodb/
 
 var NoteSchema = new mongoose.Schema({
-
-  name: String,
-  completed: { type: Boolean, default: false },
-  note: String,
-  description: String,
-  updated_at: { type: Date, default: Date.now },
-  connected_notes: []
+	
+  name: 			String,
+  completed: 		{ type: Boolean, default: false },
+  note: 			String,
+  description:		String,
+  updated_at: 		{ type: Date, default: Date.now },
+  connected_notes: 	[]
 });
 
 module.exports = mongoose.model('Note', NoteSchema);
